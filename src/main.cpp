4aa64ecc0aef53b54e8f28ca754c96058e50ffca
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-11-08 12:56:44 +0100

    diplay simple plane

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 #include <GL/glew.h> 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -41,8 +42,7 @@ int main(int argc, char* argv[]) {
 
     RenderFunction();
 
-    // Close OpenGL window and terminate GLFW
-	glfwTerminate();
+    //Cleanup();
     exit(EXIT_SUCCESS);
 }
 
@@ -65,7 +65,8 @@ void Initialize(int argc, char* argv[]) {
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    GLuint programID = LoadShaders("vertex.vs", "fragment.fs");
+    ProgramId = LoadShaders("../src/vertex.vert",
+            "../src/fragment.frag");
 
     CreateVBO();
 }
@@ -95,11 +96,12 @@ void InitWindow(int argc, char* argv[]) {
     }
 
     glfwMakeContextCurrent(window);
-    glfwSetWindowSizeCallback(window, ResizeFunction);
+    //glfwSetWindowSizeCallback(window, ResizeFunction);
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 }
 
 void ResizeFunction(GLFWwindow* window, int Width, int Height) {
+
     glfwGetWindowSize(window, &Width, &Height);
     //CurrentWidth = Width;
     //CurrentHeight = Height;
@@ -109,9 +111,28 @@ void ResizeFunction(GLFWwindow* window, int Width, int Height) {
 void RenderFunction() {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     do {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(ProgramId);
+
+        //glEnableVertexAttribArray(0);
+        //glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, 3*12, GL_UNSIGNED_BYTE, NULL);
+
+		//glEnableVertexAttribArray(0);
+		//glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
+        //glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, (void*)0);
+
+		//glDisableVertexAttribArray(1);
+		//glDisableVertexAttribArray(0);
 
         // Swap buffers
         glfwSwapBuffers(window);
@@ -120,24 +141,28 @@ void RenderFunction() {
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
             glfwWindowShouldClose(window) == 0 );
+
+	glDeleteProgram(ProgramId);
+
+	glfwTerminate();
 }
 
 void CreateVBO() {
 
     Vertex vertices[] =
     {
-        {{-1.5f, 0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{-0.5f, 0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 0.5f, 0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 1.5f, 0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{-1.5f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{-0.5f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 0.5f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 1.5f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{-1.5f, 0.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{-0.5f, 0.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 0.5f, 0.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
-        {{ 1.5f, 0.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-1.0f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-0.33f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 0.33f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 1.0f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-1.0f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-0.33f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 0.33f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 1.0f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{-0.33f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 0.33f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
+        {{ 1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}},
     };
 
     GLubyte indices[] =
@@ -156,9 +181,68 @@ void CreateVBO() {
         6, 11, 7,
     };
 
+    GLenum errorCheckValue = glGetError();
+
+    const size_t bufferSize = sizeof(vertices);
+    const size_t vertexSize = sizeof(vertices[0]);
+    const size_t rgbOffset = sizeof(vertices[0].XYZW);
+
+    glGenVertexArrays(1, &VaoId);
+    glBindVertexArray(VaoId);
+
+    glGenBuffers(1, &BufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, BufferId);
+    glBufferData(GL_ARRAY_BUFFER, bufferSize, vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, vertexSize, 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, vertexSize, (GLvoid*)
+            rgbOffset);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    glGenBuffers(1, &IndexBufferId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBufferId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+            GL_STATIC_DRAW);
+
+    errorCheckValue = glGetError();
+    if(errorCheckValue != GL_NO_ERROR) {
+        fprintf( stderr, "ERROR: Could not create a VBO: %d \n",
+                errorCheckValue);
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 void DestroyVBO() {
 
+    GLenum errorCheckValue = glGetError();
+
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, &BufferId);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, &IndexBufferId);
+
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &VaoId);
+
+    errorCheckValue = glGetError();
+    if(errorCheckValue != GL_NO_ERROR) {
+        fprintf( stderr, "ERROR: Could not destroy the VBO: %d \n",
+                errorCheckValue);
+        exit(EXIT_FAILURE);
+    }
 }
 
+void Cleanup() {
+
+    DestroyVBO();
+
+    glDeleteProgram(ProgramId);
+	glfwTerminate();
+}
