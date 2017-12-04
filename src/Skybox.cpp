7d85130e49b85e22574f6eb961ee2d28d48783db
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-12-04 11:03:36 +0100

    update error message of skybox vbo

--- src/Skybox.cpp
+++ src/Skybox.cpp
@@ -64,7 +64,7 @@ Skybox::~Skybox() {
 
     errorCheckValue = glGetError();
     if(errorCheckValue != GL_NO_ERROR) {
-        fprintf( stderr, "ERROR: Could not destroy the VBO: %d \n",
+        fprintf( stderr, "ERROR: Could not destroy the Skybox VBO: %d \n",
                 errorCheckValue);
         exit(EXIT_FAILURE);
     }
